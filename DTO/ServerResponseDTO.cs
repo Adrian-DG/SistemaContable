99341f96e23a43c26b379a6ccad24ed570ab078b
Adrian Jose Diaz Guilamo <ajdiaz@ADRIANDIAZ>
2022-02-22 13:30:14 -0200

    ServerResponse added

--- DTO/ServerResponseDTO.cs
+++ DTO/ServerResponseDTO.cs
@@ -5,5 +5,29 @@ namespace API.DTO
         public string Titulo { get; set; }
         public string Mensaje { get; set; }
         public bool Estatus { get; set; }
+
+
+        public ServerResponseDTO GetOkResponse()
+        {
+            return new ServerResponseDTO() 
+            { 
+                Titulo = "Proceso exitoso", 
+                Mensaje = "Se guardaron los cambios de forma exitosa", 
+                Estatus = true 
+            };
+        }
+
+        public ServerResponseDTO GetBadResponse()
+        {
+            return new ServerResponseDTO()
+            {
+                Titulo = "Proceso fallido",
+                Mensaje = "El proceso fallo, algo salio mal!!",
+                Estatus = false
+            };
+        }
+
+
     }
+    
 }
